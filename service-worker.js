6d02ad1368448933012d77e974d86ca99a4298a9
JiangZhaojin <jiang.zhaojin@foxmail.com>
2019-12-14 10:37:44 +0800

    update: 更新UI🌙
    Change-Id: I06a4459ba8be53b08c9c073849e35ce52bbfc067

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6f9279230f57f3b94c844ac1480e07f1.js"
+  "/precache-manifest.c63deb653d3badfb872ba6195a50c5e9.js"
 );
 
 self.addEventListener('message', (event) => {
