75bc639657df0d80267eaa6bcfc5da54a3536eca
JiangZhaojin <jiang.zhaojin@foxmail.com>
2020-03-21 15:35:02 +0800

    update links
    Change-Id: Iba505ea164570b00a7385cb458dc6049b78749a7

--- source/src/music/comment/index.tsx
+++ source/src/music/comment/index.tsx
@@ -34,14 +34,14 @@ export default class Comment extends Component<any> {
         <TipsWrapper>
           <MoneyCode><MoneyCode2></MoneyCode2></MoneyCode>
           <TipsText>
-            由于域名不能备案，只能用国外的服务器，会很慢（建议百度网盘下载
+            由于域名不能备案，只能用国外的服务器，会很慢（建议Github下载
           </TipsText>
         </TipsWrapper>
         <CommentContent id="vcomments"></CommentContent>
         <DevelopInfo>
           建议/吐槽&联系开发者：
           <DevelopLink target="_blank" href="https://weibo.com/u/6894442207">微博</DevelopLink>&nbsp;&nbsp;
-          下载：<DevelopLink target="_blank" href="https://pan.baidu.com/s/1lJXJw4iKl-lrn6wKxBCEPQ">百度网盘</DevelopLink>(提取码:mehf)
+          下载：<DevelopLink target="_blank" href="https://github.com/JiangZhaojin/ByeByeLiZhi/tree/master/music>Github</DevelopLink>
         </DevelopInfo>
       </CommentWrapper>
     )
