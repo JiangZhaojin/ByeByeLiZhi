2ed1846da35cfed01d48d2a5779b7dfa56057b92
JiangZhaoJin <jiangzhaojin@bytedance.com>
2022-10-28 16:18:54 +0800

    Update index.tsx
    update download link.

--- source/src/music/comment/index.tsx
+++ source/src/music/comment/index.tsx
@@ -31,17 +31,17 @@ export default class Comment extends Component<any> {
   render() {
     return (
       <CommentWrapper>
-        <TipsWrapper>
+<!--         <TipsWrapper>
           <MoneyCode><MoneyCode2></MoneyCode2></MoneyCode>
           <TipsText>
-            由于域名不能备案，只能用国外的服务器，会很慢（建议Github下载
+            
           </TipsText>
-        </TipsWrapper>
+        </TipsWrapper> -->
         <CommentContent id="vcomments"></CommentContent>
         <DevelopInfo>
-          建议/吐槽&联系开发者：
-          <DevelopLink target="_blank" href="https://weibo.com/u/6894442207">微博</DevelopLink>&nbsp;&nbsp;
-          下载：<DevelopLink target="_blank" href="https://github.com/JiangZhaojin/ByeByeLiZhi/tree/master/music">Github</DevelopLink>
+          由于域名不能备案，只能用国外服务器。
+<!--           <DevelopLink target="_blank" href="https://weibo.com/u/6894442207">微博</DevelopLink>&nbsp;&nbsp; -->
+          下载：<DevelopLink target="_blank" href="https://pan.baidu.com/s/1hQUZtLFxsBtoS0zbLm9qHQ">网盘(r87t)</DevelopLink>
         </DevelopInfo>
       </CommentWrapper>
     )
